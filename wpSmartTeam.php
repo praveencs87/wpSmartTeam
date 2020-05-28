fd4536064c28d3f144e1841f0e3f7c1f9ae7dd8f
praveen <praveencs87@gmail.com>
2020-05-28 20:21:40 +0530

    Initial Commit

---
+++ wpSmartTeam.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       WP SMART TEAM
+ * Description:       Created for BellCoww
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Praveen C S
+ * Author URI:        http://praveencs87.github.io/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
